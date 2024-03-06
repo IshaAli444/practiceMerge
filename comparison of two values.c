290890088c3f257e3435a3462955a9944718c9b0
Isha Ali <ishaali018@gmail.com>
2024-03-06 14:19:58 +0500

    first commit

---
+++ comparison of two values.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main ()
+{
+    int x;
+    int y;
+
+    printf ("Enter the value of x\n");
+    scanf ("%d", &x);
+
+    printf ("Enter the value of y\n");
+    scanf ("%d", &y);
+
+    int compare;
+
+    if (x<y)
+    {
+        printf ("x is less than y\n");
+    }
+    else if (x>y)
+    {
+        printf ("x is greater than y\n");
+    }
+    else if (x==y)
+    {
+        printf ("x is equal to y\n");
+    }
+
+    return 0;
+}
